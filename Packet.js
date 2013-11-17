81acc7b268d40aadbcc381bc076578b3772b8ba6
shadowcoder <root@gravirift.com>
2013-11-17 12:33:54 -0500

    Proper header implementation for clients and MDs

--- Packet.js
+++ Packet.js
@@ -5,12 +5,27 @@ function Packet(buf){
     this.length = this.readUInt16(0);
 }
 
+Packet.prototype.readUInt8 = function(){ this.offset += 1; return this.buf.readUInt8LE(this.offset-1); };
 Packet.prototype.readUInt16 = function(){ this.offset += 2; return this.buf.readUInt16LE(this.offset-2); };
 Packet.prototype.readUInt32 = function(){ this.offset += 4; return this.buf.readUInt32LE(this.offset-4); };
+Packet.prototype.readUInt64 = function(){ this.offset += 8; return this.buf.readUInt64LE(this.offset-8); };
 Packet.prototype.readBlob = function(l){ var b = new Buffer(l); this.buf.copy(b, 0, this.offset, this.offset+l); this.offset+=l; return b};
 Packet.prototype.readString = function(){ return this.readBlob(this.readUInt16()).toString(); }
 
 Packet.prototype.readClientHeader = function(){ this.msgType = this.readUInt16(); };
+Packet.prototype.readMDHeader = function(){ 
+	this.recipient_count = this.readUInt8();
+	this.recipients = [];
+	
+	var i = this.recipient_count;
+	while(i){
+		this.recipients.push(this.readUInt64());	
+		--i;
+	}
+	
+	this.sender = this.readUInt64();
+	this.msgtype = readUint16();
+};
 
 function OutPacket(){
     this.buf = [];
@@ -21,4 +36,15 @@ OutPacket.prototype.writeUInt32 = function(b){ this.writeUInt16(b & 0xFFFF); thi
 OutPacket.prototype.writeBlob = function(b,l){ var i = 0; while(i < l){ this.buf.push(b[i].charCodeAt(0)); ++i; }; };
 OutPacket.prototype.writeString = function(str){ this.writeUInt16(str.length); this.writeBlob(str,str.length);};
 
-OutPacket.prototype.serializeClient = function(){ var l = this.buf.length; return new Buffer([l & 0xFF, (l >> 8) & 0xFF].concat(this.buf));  };
+OutPacket.prototype.writeClientHeader = function(msgtype){ this.writeUInt16(msgtype); };
+OutPacket.prototype.writeMDHeader = function(recipients, sender, msgtype){
+	this.writeUInt8(recipients.length);
+	var i = 0;
+	while(i < recipients.length){
+		this.writeUInt64(recipients[i++]);
+	}
+	this.writeUInt64(sender);
+	this.writeUInt64(msgtype);
+};
+
+OutPacket.prototype.serialize = function(){ var l = this.buf.length; return new Buffer([l & 0xFF, (l >> 8) & 0xFF].concat(this.buf));  };
