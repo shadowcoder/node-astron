c3f8e6c38547cb3926622bcaff86541fa155a081
shadowcoder <root@gravirift.com>
2013-11-17 11:19:18 -0500

    Added client header and serialization routines

--- Packet.js
+++ Packet.js
@@ -3,7 +3,6 @@ function Packet(buf){
     this.offset = 0;
     
     this.length = this.readUInt16(0);
-    this.msgtype = this.readUInt16(2);
 }
 
 Packet.prototype.readUInt16 = function(){ this.offset += 2; return this.buf.readUInt16LE(this.offset-2); };
@@ -11,8 +10,9 @@ Packet.prototype.readUInt32 = function(){ this.offset += 4; return this.buf.read
 Packet.prototype.readBlob = function(l){ var b = new Buffer(l); this.buf.copy(b, 0, this.offset, this.offset+l); this.offset+=l; return b};
 Packet.prototype.readString = function(){ return this.readBlob(this.readUInt16()).toString(); }
 
-function OutPacket(type){
-    this.type = type;
+Packet.prototype.readClientHeader = function(){ this.msgType = this.readUInt16(); };
+
+function OutPacket(){
     this.buf = [];
 }
 OutPacket.prototype.writeUInt8 = function(b){ this.buf.push(b & 0xFF); };
@@ -20,3 +20,5 @@ OutPacket.prototype.writeUInt16 = function(b){ this.writeUInt8(b & 0xFF); this.w
 OutPacket.prototype.writeUInt32 = function(b){ this.writeUInt16(b & 0xFFFF); this.writeUInt16((b >> 16) & 0xFFFF); };
 OutPacket.prototype.writeBlob = function(b,l){ var i = 0; while(i < l){ this.buf.push(b[i].charCodeAt(0)); ++i; }; };
 OutPacket.prototype.writeString = function(str){ this.writeUInt16(str.length); this.writeBlob(str,str.length);};
+
+OutPacket.prototype.serializeClient = function(){ var l = this.buf.length; return new Buffer([l & 0xFF, (l >> 8) & 0xFF].concat(this.buf));  };
